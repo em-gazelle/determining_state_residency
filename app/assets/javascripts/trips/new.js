a6ff89f6fe5f7ba1b9b5cd31d2b2dd20e11af8b7
em-gazelle <emmaf9@gmail.com>
2016-11-27 13:36:07 -0500

    Add a dynamic number of trips: updated views; used JS to show/hide and render

---
+++ app/assets/javascripts/trips/new.js
@@ -0,0 +1,34 @@
+$(document).on("click", "#render_trips_form", function() {
+	$("#render_trips_form").hide();
+	$("#hidden_form").show();
+});
+
+$(document).on("click", "#render_more_trips_5", function() {
+	$("#render_more_trips_5").hide();
+	$("#hidden_form_5").show();
+});
+
+$(document).on("click", "#render_more_trips_7", function() {
+	$("#render_more_trips_7").hide();
+	$("#hidden_form_7").show();
+});
+
+$(document).on("click", "#render_more_trips_9", function() {
+	$("#render_more_trips_9").hide();
+	$("#hidden_form_9").show();
+});
+
+$(document).on("click", "#render_more_trips_11", function() {
+	$("#render_more_trips_11").hide();
+	$("#hidden_form_11").show();
+});
+
+$(document).on("click", "#render_more_trips_13", function() {
+	$("#render_more_trips_13").hide();
+	$("#hidden_form_13").show();
+});
+
+$(document).on("click", "#render_more_trips_15", function() {
+	$("#render_more_trips_15").hide();
+	$("#hidden_form_15").show();
+});
