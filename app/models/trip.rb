1ee6ff24a5292eef0b9c28e6666bb0ddbdfeaadd
em-gazelle <emmaf9@gmail.com>
2016-11-29 00:41:55 -0500

    Trip validations, error adjustments, added DS_Store to gitignore

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,12 +3,19 @@ class Trip < ActiveRecord::Base
 	
 	validates_presence_of :start_date, :end_date
 	validate :state_abbreviated
+	validate :validating_dates
 
 	before_create :set_total_days, on: [ :create ]
 
+	def validating_dates
+		if start_date.nil? || end_date.nil? || (end_date < start_date)
+			errors.add(:end_date, "must be ahead of start date")
+		end		
+	end
+
 	def state_abbreviated
 		if state.nil? || (state.upcase != state) || (state.length != 2) 
-			errors.add(:state, "Please write your state in the form of 2 abbreviated, capital letters.")
+			errors.add(:state, "must be supplied in the form of 2 abbreviated, capital letters.")
 		end
 	end
 
